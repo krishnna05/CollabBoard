1b0a2f13b3f3f605319ef93af2adcae4b39db93b
Krishna Vishwakarma <krishhnna02@gmail.com>
2026-01-10 14:51:02 +0530

    server setup

---
+++ Backend/src/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const events = require('./utils/events');
+const registerDrawHandlers = require('./handlers/drawHandler');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173", 
+      process.env.CLIENT_URL   
+    ],
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('CollabBoard Server is Running');
+});
+
+io.on(events.CONNECT, (socket) => {
+  console.log(`Client connected: ${socket.id}`);
+  registerDrawHandlers(io, socket);
+
+  socket.on(events.DISCONNECT, () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
